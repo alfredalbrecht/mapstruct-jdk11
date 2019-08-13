9757ef9cbb2d57913818666416a458d15af34042
alfred <mail@alfred-albrecht.net>
2019-08-13 11:11:33 +0200

    Add initial project

---
+++ src/main/java/com/test/mapstruct/App.java
@@ -0,0 +1,13 @@
+package com.test.mapstruct;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hallo World!" );
+    }
+}
---
+++ src/main/java/com/test/mapstruct/MyMapper.java
@@ -0,0 +1,7 @@
+package com.test.mapstruct;
+
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface MyMapper {
+}
